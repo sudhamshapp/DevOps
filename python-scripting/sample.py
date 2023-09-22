cebe165f3dbb8f66a47f0f9c6dfb5c5da701d4c0
sudhamsh <sudhamsh1996@gmail.com>
2023-09-22 21:36:08 +0530

    added features

---
+++ python-scripting/sample.py
@@ -0,0 +1,12 @@
+# def addition(x, y):
+#     print("Performing Addition")
+#     return x + y
+# result = addition(2,3)
+# print(result)
+
+def addition(x, y):
+    print("Performing Addition")
+    return x + y
+add = addition(a, b)
+print(add)
+print("\n")
